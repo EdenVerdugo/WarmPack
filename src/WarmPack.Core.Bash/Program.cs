30e91a68e87091b23fcb79e2a0a293ce684fa708
eden.verdugo@gmail.com <eden.verdugo@gmail.com>
2019-09-24 18:10:06 -0600

    se agrega el bash

---
+++ src/WarmPack.Core.Bash/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace WarmPack.Core.Bash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ooook", Color.Pink);
+        }
+    }
+}
